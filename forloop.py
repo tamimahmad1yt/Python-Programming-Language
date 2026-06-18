47d0ad5d31b08bcd1a594d4bbc676f45691316c7
TAMIM AHMAD <158311911+tamimahmad1yt@users.noreply.github.com>
2026-06-18 10:56:14 +0530

    Change print message in forloop.py to 'TAMIM W W'

--- forloop.py
+++ forloop.py
@@ -1,11 +1,11 @@
 for i in range(1,11):
-    print(" TAMIM WORLD WINNER ")
+    print(" TAMIM W W ")
             #OR
 for i in range(1,11,2):   #(start/inclusive, stop/exclusive, step(i+?))
-    print(i,"TAMIM WORLD WINNER ")
+    print(i," TAMIM W W ")
             #OR
 for i in range(10):
-    print(" TAMIM WORLD WINNER ")
+    print(" TAMIM W W ")
             #OR
 for _ in range(10):
-    print(" TAMIM WORLD WINNER ")
+    print(" TAMIM W W ")
